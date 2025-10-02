bbd1c1c641770aa13fb19980d9c4f70bb585c9b6
Max_Belonosov <st4809508@stud.duikt.edu.ua>
2025-10-02 22:09:26 +0300

    Add files via upload

---
+++ Task1/Task1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Введіть ціле число: ");
+            string? input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                Console.WriteLine($"Ваше число: {number}");
+                Console.WriteLine(GetMessage(number));
+            }
+            else
+            {
+                Console.WriteLine("Це не ціле число!");
+            }
+        }
+        
+
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static string GetMessage(int number)
+        {
+            if (IsEven(number))
+                return "Двері відкриваються!";
+            else
+                return "Двері зачинені...";
+        }
+    }
+}
+
